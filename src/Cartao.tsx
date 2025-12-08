a31a9f916ad6496739ef99fafd5ddbaf6ee51b2e
mmarianaPinho <mariana.chimenes@estudante.ifms.edu.br>
2025-12-08 08:52:48 -0400

    comit

--- src/Cartao.tsx
+++ src/Cartao.tsx
@@ -6,13 +6,14 @@ import {
   CardCvcElement 
 } from "@stripe/react-stripe-js";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import api from "./api/api";
 
 export default function CartaoPagamento() {
   const stripe = useStripe();
   const elements = useElements();
   const location = useLocation();
+  const navigate = useNavigate();
   const total = location.state?.total;
 
   const [loading, setLoading] = useState(false);
@@ -24,12 +25,14 @@ export default function CartaoPagamento() {
     setLoading(true);
 
     try {
+      
       const { data } = await api.post("/pagamento", {
-        amount: Math.round(Number(total) * 100), // em centavos
+        amount: Math.round(Number(total) * 100), 
       });
 
       const { clientSecret } = data;
 
+      
       const result = await stripe.confirmCardPayment(clientSecret, {
         payment_method: {
           card: elements.getElement(CardNumberElement)!,
@@ -39,9 +42,22 @@ export default function CartaoPagamento() {
       if (result.error) {
         setStatus("Erro: " + result.error.message);
       } else if (result.paymentIntent?.status === "succeeded") {
-        setStatus("Pagamento aprovado!");
+        setStatus("Pagamento aprovado! Limpando carrinho...");
+
+        try {
+         
+          await api.delete("/carrinho");
+
+          
+          localStorage.removeItem("filtrosCarrinho");
+        } catch (e) {
+          console.error("Erro ao limpar carrinho:", e);
+        }
+
+        
+        setTimeout(() => navigate("/carrinho"), 2000);
       }
-    } catch (e: any) {
+    } catch (e) {
       setStatus("Erro no pagamento.");
     }
 
@@ -53,7 +69,8 @@ export default function CartaoPagamento() {
   }
 
   return (
-    <div>
+    <div className="carrinho-container">
+      <button onClick={() => navigate('/carrinho')}>Voltar</button>
       <h2>Pagar R$ {total}</h2>
 
       <div className="pagamento-container">
